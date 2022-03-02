b48bdef6ef90b6ee0f0f3f7520fc6ccbef949081
lucas rego <lucasalvesrego.contato@gmail.com>
2022-03-02 18:42:54 -0300

    add: example of how a table could be created

--- exemple/exemple.php
+++ exemple/exemple.php
@@ -1,21 +1,46 @@
 <?php
 
 
-require "../vendor/autoload.php";
+require __DIR__ . "/../vendor/autoload.php";
 
-use FirebdPHP\Firebird\FireBirdPHP;
-use FirebdPHP\Firebird\TablesFirebird;
+use FirebdPHP\Firebird\QueryRunner;
+use FirebdPHP\Firebird\Table;
 
-//Create new Table
-// (new FirebirdPHP)->createTable('TESTE4', [
+// Create new Table
+// (new TablesFirebird)->createTable('TESTE4', [
 
 //     'name:varchar:255:notNull',
 //     'age:integer:notNull',
 //     'city:varchar:255:notNull',
 //     'state:varchar:255'
-    
-// ] );
+
+// ]);
+
+QueryRunner::run(
+    Table::create(
+        name: "users",
+        columns: [
+            "id" => [
+                "type" => "int",
+                "increment",
+                "primary"
+
+            ],
+            "name" => [
+                "type" => "varchar",
+                "limit" => 255
+            ],
+            "age" => [
+                "type" => "int",
+            ],
+            "city" => [
+                "type" => "varchar?",
+                "limit" => 255
+            ]
+        ]
+    )
+);
 
 
 //Show All Tables
-(new TablesFirebird)->getTablesInDatabase();
+// (new TablesFirebird)->getTablesInDatabase();
